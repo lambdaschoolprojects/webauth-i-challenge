cc336d329be7bf03037d61a452c5c0e23a63cd2c
RaneWallin <rane@ranewallin.com>
2019-06-07 20:12:20 -0700

    scaffold in login features

--- data/user-helpers.js
+++ data/user-helpers.js
@@ -3,28 +3,43 @@ const knexConfig = require("../knexfile").development;
 
 const db = knex(knexConfig);
 
+/**
+ * Gets all users in the db
+ * @returns {array} an array of all users
+ */
 const getUsers = () => {
   return db("users");
 };
 
+/**
+ * Gets a single user in the db
+ * @param {number} id
+ * @returns {object} a user with the matching id, if one exists
+ */
 const getUser = id => {
   return db("users").where({ id });
 };
 
 /**
  *
- * @param user
- * @returns id of inserted user
+ * @param {object} user
+ * @returns {number} id of inserted user
  */
 const addUser = async user => {
   const result = await db("users").insert(user);
-  console.log(result);
 
   return result[0];
 };
 
-const deleteUser = user => {
-  return null;
+/**
+ * Deletes a user from the db
+ * @param {number} id
+ * @returns {number} the number of rows deleted
+ */
+const deleteUser = async id => {
+  return await db("users")
+    .where({ id })
+    .delete();
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const helmet = require('helmet');
-const logger = require('morgan');
+require("dotenv");
+const express = require("express");
+const helmet = require("helmet");
+const logger = require("morgan");
+const session = require("express-session");
+const keys = require("./keys/keys");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
-server.use(logger('dev'));
+server.use(logger("dev"));
 
-server.get('/', (req, res) => {
-    res.send("<h1>I'm here</h1>");
-})
+server.use(
+  session({
+    name: "user-session",
+    secret: keys.cookieKey,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // one week
+      secret: true
+    },
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
+server.get("/", (req, res) => {
+  res.send("<h1>I'm here</h1>");
+});
 
 module.exports = server;
