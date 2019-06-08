8f511c2ad960d364db4440451cde9adaa73bb8cd
RaneWallin <rane@ranewallin.com>
2019-06-07 18:07:28 -0700

    scaffold users db and tests

---
+++ data/__tests__/users.js
@@ -0,0 +1,7 @@
+const db = require('../user-helpers');
+
+describe('user-helpers.js', () => {
+    it('gets users from the database', () => {
+
+    });
+});
---
+++ data/user-helpers.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile').development;
+
+const db = knex(knexConfig);
+
+module.exports = {
+    getUsers
+};
